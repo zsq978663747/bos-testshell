2a682226abdb0c1ad25e1cc74df1efc44a3d2424
zhangshiqi <978663747@qq.com>
2018-12-03 18:42:33 +0800

    modify server.pu

--- notify_plugin_test/server.py
+++ notify_plugin_test/server.py
@@ -9,9 +9,12 @@ def login():
     if request.method == 'POST':
         data = json.loads(request.data)
         #print '\n\ndata:',data["actions"]
-        js = data["actions"][0]
-        #print 'js:',js
-        str = '\"CONTRACT_NAME\" : \"{}\", \"ACTION_NAME\" : \"{}\", \"ACTION_DATA\" : \"{}\",\"IRREVERSIBLE\" : \"{}\"'.format(js["account"],js["name"],js["action_data"],data["irreversible"])
-        print '\n\n"MESSAGE" ====>',str
-        print '***********************************************************************'
+        l = len(data["actions"])
+        for i in range (0,l):
+            #print("i = %d",i)
+            js = data["actions"][i]
+            #print 'js:',js
+            str = '\"CONTRACT_NAME\" : \"{}\", \"ACTION_NAME\" : \"{}\", \"ACTION_DATA\" : \"{}\",\"IRREVERSIBLE\" : \"{}\"'.format(js["account"],js["name"],js["action_data"],data["irreversible"])
+            print '\n\n"MESSAGE" ====>',str
+            print '***********************************************************************'
     return "Hello World!"
